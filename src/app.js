981b9b884ab93a16d3faf092bbf0623a1b218beb
garritfra <garritfranke@gmail.com>
2018-11-22 22:08:08 +0100

    Add simpel TCP Server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const net = require("net");
+const server = net.createServer(c => {
+  //'connection' listener
+  console.log("client connected");
+
+  c.on("end", () => {
+    console.log("client disconnected");
+  });
+
+  c.write("Hi!");
+  c.pipe(c);
+});
+
+server.listen(42, () => {
+  //'listening' listener
+  console.log("server bound");
+});
