c4096b4a1d587c032f64405c03156aaa41fb5bb3
garritfra <garritfranke@gmail.com>
2018-12-22 16:27:13 +0100

    Add --port flag

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"strconv"
+
 	"github.com/garritfra/blockchain-project/server"
 )
 
 func main() {
-	server.ServeHTTP(":42000")
+	portFlag := flag.Int("port", 42000, "The port this service is running on")
+	flag.Parse()
+
+	var port string
+	port += ":"
+	port += strconv.Itoa(*portFlag)
+	fmt.Println(*portFlag)
+	server.ServeHTTP(port)
 }
