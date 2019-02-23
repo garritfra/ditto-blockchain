541f77ba9b622d2eaf4e94a816dce29157d26d49
garritfra <garritfranke@gmail.com>
2019-02-23 16:32:05 +0100

    Add dockerfile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
+  useEffect(() => {
+    axios.get("https://baconipsum.com/api/?type=meat-and-filler").then(res => {
+      console.log(res.data);
+    });
+  });
   return (
     <div>
       <h1>Hello from App!</h1>
